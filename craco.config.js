07623288c0342bf462a1e6ad794ba6844816a82d
realbrodiwhite <149413096+realbrodiwhite@users.noreply.github.com>
2024-09-26 21:49:08 -0700

    Add files via upload

---
+++ craco.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    webpack: {
+        configure: (webpackConfig) => {
+            // Custom Webpack configuration
+            return webpackConfig;
+        },
+    },
+    devServer: {
+        // Default configuration
+    },
+};
